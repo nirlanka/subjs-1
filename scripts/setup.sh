826808384a08df84b755f3a0332b27c03603a0be
Nir Lanka/Shane <nir3al@gmail.com>
2025-12-19 00:42:20 +0800

    Add server, remove redundant aliases

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -1,12 +1,10 @@
 #!/usr/bin/env zsh
 
-sudo chmod +x ./n
-
 sudo chmod +x ./bin/node-*/bin/node
 xattr -d com.apple.quarantine ./bin/node-*/bin/node
-
-sudo chmod +x ./x
 
 sudo chmod +x ./bin/node-*/bin/npm
 sudo chmod +x ./bin/node-*/bin/npx
 sudo chmod +x ./bin/node-*/bin/corepack
+
+sudo chmod +x ./serve
